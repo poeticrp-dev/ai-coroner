4bb013168f429352bc25f2e2797b4ab96561fe52
Poetic RP <poeticrp@gmail.com>
2025-09-13 06:31:12 -0500

    Create config.lua

---
+++ config.lua
@@ -0,0 +1,9 @@
+Config = {}
+
+Config.CoronerPed = 's_m_m_doctor_01'
+Config.CoronerVan = 'cvan'
+Config.CleanupRadius = 20.0
+
+-- Ocean Medical Hospital respawn coords
+Config.HospitalCoords = vector3(-498.5, -335.4, 34.5)
+Config.HospitalHeading = 85.0
